e29d436d2ad27c62d399d63d5f073be16f58a7f4
Overlord9001 <45371858+Overlord9001@users.noreply.github.com>
2019-03-05 11:54:22 +0100

    rettede velocity til direction

--- Pieborne/InputHandler.cs
+++ Pieborne/InputHandler.cs
@@ -30,7 +30,7 @@ namespace Pieborne
             keybinds.Add(Keys.D, new MoveCommand(new Vector2(1, 0)));
             keybinds.Add(Keys.A, new MoveCommand(new Vector2(-1, 0)));
             keybinds.Add(Keys.W, new JumpCommand());
-            keybinds.Add(Keys.Space, new MoveCommand(new Vector2(0, 1)));
+            keybinds.Add(Keys.Space, new JumpCommand());
 
         }
 
