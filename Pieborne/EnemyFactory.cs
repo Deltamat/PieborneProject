fd63f4ec749ea583eb59781417805a85b6e8e91c
Mathias <Mathias@LAPTOP-9FSN3NE0>
2019-03-05 12:45:29 +0100

    factory

---
+++ Pieborne/EnemyFactory.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pieborne
+{
+    class EnemyFactory : Factory
+    {
+        static EnemyFactory instance;
+        public static EnemyFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new EnemyFactory();
+                }
+                return instance;
+            }
+        }
+
+        private EnemyFactory()
+        {
+
+        }
+
+        public override GameObject Create(string type, Vector2 startPos)
+        {
+            GameObject gameObject = new GameObject();
+
+            switch(type)
+            {
+                case "Rat":
+                    break;
+
+                case "RangedRat":
+                    break;
+            }
+
+            gameObject.Transform.Position = startPos;
+            return gameObject;
+        }
+    }
+}
---
+++ Pieborne/TerrainFactory.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pieborne
+{
+    class TerrainFactory : Factory
+    {
+        static TerrainFactory instance;
+        static public TerrainFactory Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new TerrainFactory();
+                }
+                return instance;
+            }
+        }
+
+        private TerrainFactory()
+        {
+
+        }
+
+        public override GameObject Create(string type, Vector2 startPos)
+        {
+            GameObject gameObject = new GameObject();
+
+            switch(type)
+            {
+                case "Brick":
+                    gameObject.AddComponent(new Terrain());
+                    gameObject.AddComponent(new SpriteRenderer("bricks/Brick Black"));
+                    gameObject.AddComponent(new Collider());
+                    break;
+            }
+            gameObject.Transform.Position = startPos;
+            return gameObject;
+        }
+    }
+}
